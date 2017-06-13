6e9d5df8ed3d927f2a2322847d0c8fe649902022
Francisco Moreno <famoreno@uma.es>
2017-06-13 16:09:22 +0200

    Add License file

--- src/CStereoSLAMKF.cpp
+++ src/CStereoSLAMKF.cpp
@@ -1,37 +1,25 @@
-/* +---------------------------------------------------------------------------+
-   |                 The Mobile Robot Programming Toolkit (MRPT)               |
-   |                                                                           |
-   |                          http://www.mrpt.org/                             |
-   |                                                                           |
-   | Copyright (c) 2005-2013, Individual contributors, see AUTHORS file        |
-   | Copyright (c) 2005-2013, MAPIR group, University of Malaga                |
-   | Copyright (c) 2012-2013, University of Almeria                            |
-   | All rights reserved.                                                      |
-   |                                                                           |
-   | Redistribution and use in source and binary forms, with or without        |
-   | modification, are permitted provided that the following conditions are    |
-   | met:                                                                      |
-   |    * Redistributions of source code must retain the above copyright       |
-   |      notice, this list of conditions and the following disclaimer.        |
-   |    * Redistributions in binary form must reproduce the above copyright    |
-   |      notice, this list of conditions and the following disclaimer in the  |
-   |      documentation and/or other materials provided with the distribution. |
-   |    * Neither the name of the copyright holders nor the                    |
-   |      names of its contributors may be used to endorse or promote products |
-   |      derived from this software without specific prior written permission.|
-   |                                                                           |
-   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       |
-   | 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED |
-   | TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR|
-   | PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE |
-   | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL|
-   | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR|
-   |  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)       |
-   | HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       |
-   | STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  |
-   | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           |
-   | POSSIBILITY OF SUCH DAMAGE.                                               |
-   +---------------------------------------------------------------------------+ */
+/*********************************************************************************
+**   				SRBA-Stereo-SLAM    				**
+**********************************************************************************
+**										**
+**	Copyright(c) 2015-2017, Jose Luis Blanco, University of Almeria         **
+**	Copyright(c) 2015-2017, Francisco-Angel Moreno 				**
+**			MAPIR group, University of Malaga			**
+**	All right reserved
+**										**
+**  This program is free software: you can redistribute it and/or modify	**
+**  it under the terms of the GNU General Public License (version 3) as		**
+**	published by the Free Software Foundation.				**
+**										**
+**  This program is distributed in the hope that it will be useful, but		**
+**	WITHOUT ANY WARRANTY; without even the implied warranty of		**
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the		**
+**  GNU General Public License for more details.				**
+**										**
+**  You should have received a copy of the GNU General Public License		**
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.	**
+**										**
+**********************************************************************************/
 #include "CStereoSLAMKF.h"
 
 extern TGeneralOptions general_options;
